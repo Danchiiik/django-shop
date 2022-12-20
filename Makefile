be8a0a4b39967aa2343e81c2af7e4cc55c74baf0
Danchiiik <dcabatar@gmail.com>
2022-12-20 21:19:47 +0600

    hello

---
+++ Makefile
@@ -0,0 +1,8 @@
+run:
+	./manage.py runserver
+migrate:
+	./manage.py makemigrations
+	./manage.py migrate
+user:
+	./manage.py createsuperuser
+
---
+++ applications/account/send_mail.py
@@ -0,0 +1,22 @@
+from django.core.mail import send_mail
+
+
+def send_confirmation_email(email, code):
+    import time
+    time.sleep(5)
+    full_link = f'http://localhost:8000/account/activate/{code}'
+    send_mail(
+        'Активация пользователя',
+        full_link,
+        'dcabatar@gmail.com',
+        [email]
+    )
+
+
+def send_confirmation_code(email, code):
+    send_mail(
+        'Восстановление пароля',
+        code,
+        'dcabatar@gmail.com',
+        [email]
+    )
